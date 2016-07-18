95da843619d1129a158546def9b39e154649204d
Facu Ahumada <facundoahumada93@hotmail.com>
2016-07-18 02:14:59 -0300

    Cambie el nombre de la funcion imprimeAlumnos y saque la declaracion de las variables

--- facu/prueba.c
+++ facu/prueba.c
@@ -5,18 +5,16 @@ typedef struct{
 	int anioNac;
 }alumno;
 
-void imprimeAlumnos(alumno unAlumno){
-	char inicial = unAlumno.inicial;
-	int anioNac = unAlumno.anioNac;
-	printf("inicial: %c\n", inicial);
-	printf("anio de nacimiento: %d\n", anioNac);
+void imprimirAlumnos(alumno unAlumno){
+	printf("inicial: %c\n", unAlumno.inicial);
+	printf("anio de nacimiento: %d\n", unAlumno.anioNac);
 }
 
 void main(){
     alumno yo;
     yo.anioNac = 1996;
     yo.inicial = 'm';
-    imprimeAlumnos(yo);
+    imprimirAlumnos(yo);
     alumno vos;
-    imprimeAlumnos(vos);
+    imprimirAlumnos(vos);
 }
